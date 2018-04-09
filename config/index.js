c66406514521168df7ee162e867fe49b646cf4a9
nlovesye <894765963@qq.com>
2018-04-09 21:54:46 +0800

    ns

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://192.168.0.247:7004',
+        target: 'http://119.23.238.32:8081',
         pathRewrite: {
           '^/api': '/api'
         }
