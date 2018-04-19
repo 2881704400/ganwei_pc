f0ca6ee301c32c02b32113ecc2a014ff3a178666
nlovesye <894765963@qq.com>
2018-04-19 17:53:24 +0800

    0419

--- config/index.js
+++ config/index.js
@@ -11,13 +11,13 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://192.168.0.210:8079',//'http://192.168.0.247:7001',
+        target: 'http://192.168.0.247:7001',//'http://192.168.0.247:7001',
         pathRewrite: {
           '^/api': '/api'
         }
       },
       '/oApi': {
-        target: 'http://192.168.0.196:7006',
+        target: 'http://192.168.0.196:8013',
         pathRewrite: {
           '^/oApi': '/'
         }
@@ -25,7 +25,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '192.168.0.210', // can be overwritten by process.env.HOST
+    host: '192.168.0.247', // can be overwritten by process.env.HOST
     port: 7070, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
