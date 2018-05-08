81cff89e50c94148d1d7d18293ee2704d658b7c3
zhengkanxia <2217382066@qq.com>
2018-05-08 18:03:32 +0800

    flower

--- config/index.js
+++ config/index.js
@@ -56,7 +56,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/2.0/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
