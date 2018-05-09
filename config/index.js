d94e6b3cb12bfc40f3b32a89f5b6a6b8d176e645
nlovesye <894765963@qq.com>
2018-05-09 17:59:07 +0800

    ns

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:7011',//'http://192.168.0.247:7001',
+        target: 'http://localhost:7001',
         pathRewrite: {
           '^/api': '/api',
         }
@@ -22,7 +22,7 @@ module.exports = {
     },
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8013, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 7011, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
