0c79a310541be44f43e60a711d8f0c4846b07c19
nlovesye <894765963@qq.com>
2018-05-10 13:33:21 +0800

    ns

--- config/index.js
+++ config/index.js
@@ -53,12 +53,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/2.0/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsSubDirectory: '2.0/static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
