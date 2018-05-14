2792aa91489bb5ecf3b3ba3cb049c5eea60fd0a0
zhengkanxia <2217382066@qq.com>
2018-05-14 18:08:37 +0800

    123

--- config/index.js
+++ config/index.js
@@ -11,18 +11,21 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:7003',
+        target: 'http://localhost:8013',
         pathRewrite: {
           '^/api': '/api',
         }
       },
       '/': {
-        target: 'http://localhost:7003'
+        target: 'http://localhost:8013',
+        pathRewrite: {
+          '^/': '/',
+        }
        }
     },
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 7007, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8013, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
