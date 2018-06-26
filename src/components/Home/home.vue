96ea784afc9bae29fd579898aba534b313903f66
zhengkanxia <2217382066@qq.com>
2018-06-26 17:01:08 +0800

    6/26

--- src/components/Home/home.vue
+++ src/components/Home/home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-       <h1 v-text="projectName"></h1>
-       <div class="homeContent">
-           <div class="homeTop">
+    <h1 v-text="projectName"></h1>
+    <ul class="homeContent">
+      <li v-for="item in menuType">
+        <h5 class="navigationTitle">{{item}}</h5>
+        <div class="homeMenu">
+            <span v-for="item_child in menuNameArray" v-if="(item == item_child.modulName)&&item_child.isSelect">
+              <!-- homeMenuClick 临时，按照左侧菜单方法调用 -->
+              <a @click="homeMenuClick(rootSave,item_child_in)" v-for="item_child_in in rootSave" v-show="item_child_in.node.title==item_child.menuName"><i :class="item_child.iconName"></i>{{item_child.menuName}}</a>
+            </span>
+            <span @click.stop="openModal(item)" class="alertBtn"></span>
+        </div>
+      </li>               
+    </ul>
+    <Modal v-model="menu_modal" :title="alertTitle"  class="Modalmenu"  :mask-closable="false">
+      <ul class="alertMenu">
+            <li v-for="(modul_item,index) in menuNameArray" :key="index" v-if="alertTitle == modul_item.modulName">
+                <Checkbox  v-model="checkoutSelect[index]" :id='"checkConf_"+index'  @on-change="">{{modul_item.menuName}}</Checkbox>
+            </li>
+        </ul>
+        <div slot="footer">
+            <Button type="text" size="large" @click="cancalUpdateMenu">取消</Button>
+            <Button type="primary" size="large" @click="saveUpdateMenu(alertTitle)" id="user_ok" :disabled="false">确定</Button>
+        </div>                        
+    </Modal>
 
-           </div>
-           <div class="homeMenu">
-             <span v-for="item in rootSave" v-show="!item.parent">
-               <a @click="homeMenuClick(rootSave,item)" v-show="item.node.title != '首页'"><i :class="item.node.iconClass"></i>{{item.node.title}}</a>
-             </span>
-           </div>
-       </div>
-</div>
-
+  </div>
 </template>
 
 <script>
@@ -22,11 +35,17 @@ export default {
   data () {
     return {
       projectName:'',
+      menu_modal: false,
+      alertTitle: '',
+      menuType: [],
+      menuNameArray: [],
+      menuStatusArray: [],
+      checkoutSelect:[],
     }
   },
   mounted() {
      this.getProjectNmae();
-
+     this.getMenu();
 		},
   methods:{
     homeMenuClick(root,item){
@@ -37,7 +56,6 @@ export default {
         isHome: false,
         name: data.title
       }
-     
       if (data.selected) return false
       root.forEach(ele => {
         if (ele.node.expand) ele.node.expand = false
@@ -61,17 +79,181 @@ export default {
       console.log("success");
     },
     getProjectNmae: function(){
-      var dthis = this;
+      var dt = this;
       let urlna = "/api/real/equip_tree";
       this.Axios.post(urlna).then(response => {
             if(response.data.HttpData.code == 200)
-              dthis.projectName =response.data.HttpData.data.Name;
+              dt.projectName =response.data.HttpData.data.Name;
             else
-              dthis.getProjectNmae();
+              dt.getProjectNmae();
         })
         .catch(error => {
-          // console.log(error);
         });
+    },
+    openModal(value){
+        var h=0,dt = this;
+        this.menu_modal = true;
+        this.alertTitle = value;
+        this.menuNameArray.forEach(function(el,index){
+           dt.checkoutSelect[index] = el.isSelect;
+        });
+    },
+    cancalUpdateMenu(){
+      this.menu_modal = false;
+      },
+    saveUpdateMenu(value){
+      this.delMenu(value);
+      this.menu_modal = false;
+      var url,data,dt = this; 
+      url = "set_InsertNewTable";
+      data = {
+      tableName: "GW_HOME_Menu_Status(userName,modulName,status)",
+      tableVlue: " values('" + window.localStorage.getItem('login_msg') +"','"+ value +"','"+ dt.transformBoolean(value)+"')",
+      };         
+      this.XHRPost(url,data,_success);
+      function _success(response) {
+        let arrayLike = response.data.HttpData.data;
+        let code = response.data.HttpData.code;
+        if(code == 200)
+        {        
+            dt.checkoutSelect.forEach(function(el,index){
+              dt.menuNameArray[index].isSelect = el;
+            });
+        }
+        else
+        { 
+            dt.saveUpdateMenu();
+        }
+      }
+    },
+    delMenu(value){
+      var url = "set_DeleteTableData",data={tableName: "GW_HOME_Menu_Status",tableVlue:" userName ='" + window.localStorage.getItem('login_msg') +"' and  modulName='"+ value + "'",};
+      this.XHRPost(url,data,_success);
+      function _success(response) {
+        
+      }
+    },
+    //返回菜单
+    getMenu: function() {
+      var dt = this;
+      let url = "/api/GWServiceWebAPI/SelectData?tableName=GW_HOME_Menu";
+      this.XHRGet(url, _success);
+      function _success(response) {
+        dt.menuNameArray.length = 0;
+        let arrayLike = response.data.HttpData.data;
+        let code = response.data.HttpData.code;
+        if(code == 200)
+        {        
+            let getmenuNameArrayLength = arrayLike.length;
+            for (var i = 0; i < getmenuNameArrayLength; i++) {
+              let menuNameArray_data = {
+                groupID: arrayLike[i].groupID,
+                modulName: arrayLike[i].modulName,
+                menuName: arrayLike[i].menuName,
+                iconName: arrayLike[i].iconName,
+                isSelect: false,
+              };
+              dt.menuNameArray.push(menuNameArray_data);
+              dt.menuType.push(arrayLike[i].modulName);
+            }
+            dt.getStatusMenu();
+            dt.menuType=Array.from(new Set(dt.menuType));
+        }
+        else
+        { 
+            dt.getMenu();
+        }
+      }
+    },
+    //设置状态
+    getStatusMenu: function() {
+      var dt = this;
+      let url = "get_DataByTableName";
+      let data = {TableName: " GW_HOME_Menu_Status where userName='" + window.localStorage.getItem('login_msg') + "'",}
+      this.XHRPost(url, data,_success);
+      function _success(response) {
+        dt.menuStatusArray.length = 0;
+        let arrayLike = response.data.HttpData.data;
+        let code = response.data.HttpData.code; 
+        if(code == 200)
+        {           
+            let getmenuStatusArrayLength = arrayLike.length;
+            for (var i = 0; i < getmenuStatusArrayLength; i++) {
+              if(arrayLike[i].userName ==window.localStorage.getItem('login_msg'))
+              {
+                let menuStatusArray_data = {
+                  userName: arrayLike[i].userName,
+                  modulName: arrayLike[i].modulName,
+                  status: arrayLike[i].status,
+                };
+                dt.menuStatusArray.push(menuStatusArray_data);
+              }
+            }
+            //处理menuNameArray
+            arrayLike.length==0?"":dt.init_menuNameArray();
+        }
+        else
+        { 
+            dt.getStatusMenu();
+        }
+      }
+    },    
+    //发送请求
+    XHRPost: function(api, updateJson, _success) {
+      var msg = this.$Message;
+      let urlna = "/api/GWServiceWebAPI/" + api;
+      this.Axios.post(urlna, updateJson, {
+        headers: { "Content-type": "application/json" }
+      })
+        .then(response => {
+          // msg.success("操作成功");
+          _success(response); 
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    XHRGet: function(url, _success) {
+      var msg = this.$Message;
+      this.Axios({
+        method: "get",
+        url: url,
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded"
+        }
+      })
+        .then(response => {
+          // msg.info("执行成功");
+          _success(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },  
+    init_menuNameArray(){
+      var h,that = this;
+       that.menuStatusArray.forEach(function(el,index){
+          h = 0;
+          that.menuNameArray.forEach(function(el_in,index_in){
+               if(el.modulName == el_in.modulName)
+               {
+                  el.status[h] == 1? el_in.isSelect = true: el_in.isSelect = false; 
+                  h++;
+               }
+          });         
+       });
+    },
+    transformBoolean(value){
+      var transformstringValue="";
+      var dt = this;
+      dt.menuNameArray.forEach(function(el,index){
+        if(el.modulName == value) 
+         {
+           transformstringValue += dt.checkoutSelect[index] === true?'1':'0';
+         }
+      });
+ console.log(transformstringValue);
+      return transformstringValue;
     },
   },
 
