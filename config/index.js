855ba4cf088020eb5805e789be55442283d03567
2881704400 <2881704400@qq.com>
2018-11-28 17:46:42 +0800

    合并 zkx 报警排版

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     },
     // Various Dev Server settings
 
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '192.168.0.128', // can be overwritten by process.env.HOST
     port: 7008, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
 
     autoOpenBrowser: true,
